ef3887d0b0207bad7e8d0a7e16fed9d62346f900
Ayman <aymanm95@gmail.com>
2016-11-12 14:40:06 +0000

    Tidied code by putting them into functions and improved success rate to 98.1% but to achieve 100%, I should do several passes over failed CAPTCHAs

--- opencvtest.py
+++ opencvtest.py
@@ -1,74 +1,117 @@
 import numpy as np
 import cv2
 import time
-start_time = time.time()
+import os 
 
+# Usage:
+# pycaptcha input_image method acceptance_threshold intensity_threshold 
+# output: 3 letter string 
 
 
 #methods = ['cv2.TM_CCOEFF_NORMED','cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF_NORMED']
 
-templates = []
-image = cv2.imread('110617642vkvfwq.jpg',0)
+
+
+#for file in files:
+#	print str(file)
+
+start_time = time.time()
+
 
 #binarization
+INTENSITY_THRESHOLD = 155
+#reject matches below 
+THRESHOLD = 0.87
 
-bin_img = image.copy()
-height, width = image.shape[:2]
-intensity_threshold = 155
+templates = []
 
-for x in range(height):
-	for y in range(width):
-		if image[x][y] <= intensity_threshold:
-			bin_img[x][y] = 0
-		else:
-			bin_img[x][y] = 255
-			
-image = bin_img
 
 for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
 	templates.append(letter + '.png')
 
-#reject matches below 88%
-threshold = 0.88
-
-#identified characters 
-positions = []
-
-for character, template in enumerate(templates):
+# Binarizes an image based on intensity threshold
+def binarize(image, threshold):
+	height, width = image.shape[:2]
+	bin_img = image.copy()
+	for x in range(height):
+		for y in range(width):
+			if image[x][y] <= threshold:
+				bin_img[x][y] = 0
+			else:
+				bin_img[x][y] = 255
+	return bin_img;
 	
-	# 'I' is a special case
-	if character != 8:
+# Print the matches for each character 
+def print_matches(matches):
+	for match in matches:
+		print match[0], match[1]	
+	
+# Solves the CAPTCHA and returns a 3 character string
+def solve_captcha(image, threshold):
+
+	#identified characters 
+	positions = []
+	matches = []
+	
+	height, width = image.shape[:2]
+	image = image[0:height,30:width-60]		
+	image = binarize(image,INTENSITY_THRESHOLD)
+
+	for character, template in enumerate(templates):
+	
+		# Read each template from the character set 
 		template = cv2.imread(template)
 
 		# Convert to grayscale
-		#imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+		template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
 		# Find template
-		result = cv2.matchTemplate(image,templateGray, cv2.TM_CCOEFF_NORMED)
+		result = cv2.matchTemplate(image,template_gray, cv2.TM_CCOEFF_NORMED)
 		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 		
-		top_left = max_loc
+		#top_left = max_loc
 		#h,w = templateGray.shape
 		#bottom_right = (top_left[0] + w, top_left[1] + h)
 		#cv2.rectangle(image,top_left, bottom_right,(0,0,255),1)
+		
+		# Reject results beold THRESHOLD %
 		if (max_val > threshold):
-			positions.append([top_left[0], chr(65+character)])
-			print chr(65+character), max_val
-# sort by x-position 
-positions.sort()
-string = ""
-for pair in positions:
-	string += pair[1]
+			positions.append([max_loc[0], chr(65+character)])
+			
+		matches.append([chr(65+character), max_val])
 
-print string
+	# Sort character matches by x-position for correct string  
+	positions.sort()
+	captcha = ""
+	for pair in positions:
+		captcha += pair[1]
+	
+	return matches, captcha;
+
+	
+folder = "captchas"
+files = os.listdir("captchas")
+
+total_captchas = len(files)
+failed = 0
+
+
+
+for file in files:
+	image = cv2.imread(os.path.join(folder,file),0)
+	matches, result = solve_captcha(image,THRESHOLD)
+	#print os.path.join(folder,file), os.path.join(folder,result+'.jpg')
+	#os.rename(os.path.join(folder,file), os.path.join(folder,result+'.jpg'))
+	if len(result) != 3:
+		failed += 1
+		print_matches(matches)
+		print result 
+
+accuracy = 100 * ((total_captchas - failed)/float(total_captchas))
+print "Accuracy: %.2f%% " % accuracy
 print("Time: %s seconds" % (time.time() - start_time))
 	
-
- 
 # Show result
-cv2.imshow("Image", image)
-cv2.moveWindow("Template", 10, 50);
-
-
-cv2.waitKey(0)
+#cv2.imshow("Image", image)
+#cv2.moveWindow("Template", 10, 50);
+#cv2.waitKey(0)
